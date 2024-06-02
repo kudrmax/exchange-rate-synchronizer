99a82096edf69cd7e0765d83ab65e24f3ac70d93
kudr.max <mdkudryashov@edu.hse.ru>
2024-06-02 23:46:25 +0300

    Improve schemas

--- schemas.py
+++ schemas.py
@@ -3,6 +3,9 @@ from datetime import date
 
 
 class CurrencyRateBase(BaseModel):
+    """
+    Класс для курсов валют
+    """
     currency: str  # название валюты (например 'USD')
     date: date  # дата, в которой был данный курс валют
     rate: float  # значение курса валют
@@ -15,6 +18,16 @@ class CurrencyRateCreate(CurrencyRateBase):
     pass
 
 
+class CurrencyRateUpdate(CurrencyRateBase):
+    """
+    Класс для изменения объекта курса валюты
+    """
+    id: int
+    currency: str | None = None  # название валюты (например 'USD')
+    date: date | None = None  # дата, в которой был данный курс валют
+    rate: float | None = None  # значение курса валют
+
+
 class CurrencyRateRead(CurrencyRateBase):
     """
     Класс для чтения (получения) объекта курса валюты
@@ -23,6 +36,9 @@ class CurrencyRateRead(CurrencyRateBase):
 
 
 class CountryCurrencyBase(BaseModel):
+    """
+    Класс для информации о валютах стран
+    """
     country: str  # например 'Россия'
     currency_name: str  # например 'Российский рубль'
     currency_code: str | None = None  # например 'RUB'
@@ -30,4 +46,25 @@ class CountryCurrencyBase(BaseModel):
 
 
 class CountryCurrencyCreate(CountryCurrencyBase):
+    """
+    Класс для создания информации о валютах стран
+    """
     pass
+
+
+class CountryCurrencyUpdate(CountryCurrencyBase):
+    """
+    Класс для изменения информации о валютах стран
+    """
+    id: int
+    country: str | None = None  # например 'Россия'
+    currency_name: str | None = None  # например 'Российский рубль'
+    currency_code: str | None = None  # например 'RUB'
+    currency_number: int | None = None  # например 643
+
+
+class CountryCurrencyRead(CountryCurrencyBase):
+    """
+    Класс для чтения (просмотра) информации о валютах стран
+    """
+    id: int
