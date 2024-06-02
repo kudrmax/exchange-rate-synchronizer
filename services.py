e4445f17cdda92a7b17321d6f83906b07308622b
kudr.max <mdkudryashov@edu.hse.ru>
2024-06-02 18:54:38 +0300

    Improve different currency

--- services.py
+++ services.py
@@ -16,34 +16,37 @@ def parse_rates(start_date: date, end_date: date) -> List[CurrencyRateCreate]:
         'CNY': 52207,  # китайский юань
     }
 
-    cur = 52170
-
-    url_fstring = "https://www.finmarket.ru/currency/rates/?id=10148&pv=1&cur={cur}&bd={start_day}&bm={start_month}&by={start_year}&ed={end_day}&em={end_month}&ey={end_year}&x=48&y=13#archive"
-    url = url_fstring.format(
-        cur=cur,
-        start_day=start_date.day, start_month=start_date.month, start_year=start_date.year,
-        end_day=end_date.day, end_month=end_date.month, end_year=end_date.year
-    )
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+    rates_dict = {}
 
     rates = []
-    table = soup.find('table', {'class': 'karramba'})
-    if table:
-        rows = table.find('tbody').find_all('tr')
-        for row in rows:
-            columns = row.find_all('td')
-            if len(columns) >= 3:  # @todo
-                date_text = columns[0].text.strip()
-                rate_text = columns[2].text.strip()
-                try:
-                    rate_value = float(rate_text.replace(',', '.'))
-                    rate_date = datetime.strptime(date_text, "%d.%m.%Y")
-                    rate = CurrencyRateCreate(currency='USD', date=rate_date, rate=rate_value)
-                    rates.append(rate)
-                    print(rate_date, rate_value)
-                except ValueError as ex:
-                    print(f"Error parsing row {row}: {ex}")
+
+    for curr_name, curr_code in currs.items():
+
+        url_fstring = "https://www.finmarket.ru/currency/rates/?id=10148&pv=1&cur={cur}&bd={start_day}&bm={start_month}&by={start_year}&ed={end_day}&em={end_month}&ey={end_year}&x=48&y=13#archive"
+        url = url_fstring.format(
+            cur=curr_code,
+            start_day=start_date.day, start_month=start_date.month, start_year=start_date.year,
+            end_day=end_date.day, end_month=end_date.month, end_year=end_date.year
+        )
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        table = soup.find('table', {'class': 'karramba'})
+        if table:
+            rows = table.find('tbody').find_all('tr')
+            for row in rows:
+                columns = row.find_all('td')
+                if len(columns) >= 3:  # @todo
+                    date_text = columns[0].text.strip()
+                    rate_text = columns[2].text.strip()
+                    try:
+                        rate_value = float(rate_text.replace(',', '.'))
+                        rate_date = datetime.strptime(date_text, "%d.%m.%Y")
+                        rate = CurrencyRateCreate(currency=curr_name, date=rate_date, rate=rate_value)
+                        rates.append(rate)
+                        print(rate_date, rate_value)
+                    except ValueError as ex:
+                        print(f"Error parsing row {row}: {ex}")
     return rates
 
 
