5f5c5b363872ee2d0d7db719d20449feca02490a
kudr.max <mdkudryashov@edu.hse.ru>
2024-06-05 01:48:22 +0300

    Refactore API and crud (good code)

--- crud.py
+++ crud.py
@@ -5,8 +5,7 @@ from typing import List, Optional
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
-import models
-import schemas
+from schemas import *  # @todo
 from models import *  # @todo
 from schemas import CurrencyRateCreate, CountryCreate
 
@@ -20,19 +19,35 @@ def get_currency_rates(db: Session, start_date: date, end_date: date):
     Функция для получения курсов валют из базы данных за указанный диапазон дат.
     """
     # @todo добавить значения по умолчанию для полчения всех возможных баз данных
-    return db.query(models.CurrencyRateModel).filter(
-        models.CurrencyRateModel.date >= start_date,
-        models.CurrencyRateModel.date <= end_date
+    return db.query(CurrencyRateModel).filter(
+        CurrencyRateModel.date >= start_date,
+        CurrencyRateModel.date <= end_date
+    ).all()
+
+
+def get_related_currency_rates(db: Session, start_date: date, end_date: date):
+    """
+    Функция для получения курсов валют из базы данных за указанный диапазон дат.
+    """
+    # @todo добавить значения по умолчанию для полчения всех возможных баз данных
+    return db.query(RelatedCurrencyRateModel).filter(
+        RelatedCurrencyRateModel.date >= start_date,
+        RelatedCurrencyRateModel.date <= end_date
     ).all()
 
 
 def sync_currency_rates(
         db: Session,
-        rates_to_sync: List[schemas.CurrencyRateUpdate]
+        start_date: date,
+        end_date: date,
 ):
     """
     Функция для сохранения курсов валют в базу.
     """
+
+    parser = RateParser()
+    rates_to_sync = parser.parse(start_date, end_date)
+
     was_updated_counter = 0
     was_created_counter = 0
 
@@ -59,68 +74,116 @@ def sync_currency_rates(
     return was_updated_counter, was_created_counter
 
 
-def sync_and_get_currency_related_rates(
+def sync_related_currency_rates(
         db: Session,
-        rates_to_sync: List[schemas.CurrencyRateUpdate]
+        start_date: date,
+        end_date: date,
 ):
-    was_updated_counter = 0
-    was_created_counter = 0
+    was_updated_counter, was_created_counter = 0, 0
 
-    for rate_to_sync in rates_to_sync:
-        # query = select(RelatedCurrencyRateModel).where(RelatedCurrencyRateModel.date == rate_to_sync.date).where(
-        #     RelatedCurrencyRateModel.currency_code == rate_to_sync.currency_code)
-        # result = db.execute(query)
-        # rate: RelatedCurrencyRateModel = result.scalar_one_or_none()
+    # обновление курсов валют
+    sync_currency_rates(db, start_date, end_date)
+    related_rates_to_sync = get_currency_rates(db, start_date, end_date)
 
-        # получить базовое значение rate
-        # если оно пустое, то синхронизировать
+    # обновление БД относительных курсов валют
+    for related_rate in related_rates_to_sync:
+        currency_code = related_rate.currency_code
+        param: Parameters = db.query(Parameters).get(currency_code)
+        if param:
+            related_rate_value = related_rate.rate / param.base_rate
+            query = select(RelatedCurrencyRateModel).where(
+                RelatedCurrencyRateModel.date == related_rate.date).where(
+                RelatedCurrencyRateModel.currency_code == currency_code)
+            result = db.execute(query)
+            existing_related_rate: RelatedCurrencyRateModel = result.scalar_one_or_none()
 
-        sync_base_rates(db)
-
-        # получить базовое значение rate
-        # получить из пришедшего rate и базового значения related_rate
-        # обновить или добавить
-
-        # if rate:
-        #     update_currency_rate(db, currency_code=rate.currency_code, curr_date=rate.date, new_rate=rate_to_sync)
-        #     was_updated_counter += 1
-        # else:
-        #     new_rate = CurrencyRateCreate(**rate_to_sync.model_dump())
-        #     create_currency_rate(db, new_rate=new_rate)
-        #     was_created_counter += 1
-        pass
+            if existing_related_rate:
+                update_object(
+                    db=db,
+                    model=RelatedCurrencyRateModel,
+                    obj_id=(currency_code, related_rate.date),
+                    schema=CurrencyRelatedRateUpdate(
+                        related_rate=related_rate_value
+                    )
+                )
+                was_updated_counter += 1
+            else:
+                create_object(
+                    db=db,
+                    model=RelatedCurrencyRateModel,
+                    schema=CurrencyRelatedRateCreate(
+                        currency_code=currency_code,
+                        date=related_rate.date,
+                        related_rate=related_rate_value
+                    )
+                )
+                was_created_counter += 1
     return was_updated_counter, was_created_counter
 
 
-def sync_base_rates(db: Session, base_date):
-    parser = RateParser()
-    rates_to_sync = parser.parse(base_date, base_date)
-    sync_currency_rates(db, rates_to_sync=rates_to_sync)
-    rates_from_db = get_currency_rates(db, base_date,
-                                       base_date)  # @todo сделать так, чтобы get обязательно перед этим делал sync
-    print(rates_from_db)
+def sync_base_currency_rates(
+        db: Session,
+        start_date: date,
+        end_date: date,
+):
+    was_created_counter = 0
+    was_updated_counter = 0
 
+    base_date = date(2020, 1, 1)  # @todo вынести в config
+    sync_currency_rates(db, start_date, end_date)
 
-##################################
+    currency_codes = {
+        'USD': 52148,  # доллар
+        'EUR': 52170,  # евро
+        'GBP': 52146,  # фунт стерлигов
+        'JPY': 52246,  # японская йена
+        'TRY': 52158,  # турецкая лира
+        'INR': 52238,  # индийская рупия
+        'CNY': 52207,  # китайский юань
+    }
 
+    for currency_code, _ in currency_codes.items():
 
-# def get_country_by_id(
-#         db: Session,
-#         country_id: int
-# ) -> Optional[CountryModel]:
-#     query = select(CountryModel).where(CountryModel.id == country_id)
-#     result = db.execute(query)
-#     rate: CountryModel = result.scalar_one_or_none()
-#     return rate
+        query = select(CurrencyRateModel).where(CurrencyRateModel.date == base_date).where(
+            CurrencyRateModel.currency_code == currency_code)
+        result = db.execute(query)
+        currency: CurrencyRateModel = result.scalar_one_or_none()
+
+        param: Parameters = db.query(Parameters).get(currency_code)
+        if param:
+            new_parameter = ParameterUpdate(
+                base_rate=currency.rate,
+                date_of_base_rate=base_date,
+            )
+            update_object(
+                db=db,
+                model=Parameters,
+                obj_id=currency_code,
+                schema=new_parameter
+            )
+            was_updated_counter += 1
+        else:
+            new_parameter = Parameter(
+                currency_code=currency_code,
+                base_rate=currency.rate,
+                date_of_base_rate=base_date,
+            )
+            create_object(
+                db=db,
+                model=Parameters,
+                schema=new_parameter
+            )
+            was_created_counter += 1
+    return was_updated_counter, was_created_counter
 
 
 def get_countries(db: Session):
-    return db.query(models.CountryModel).all()
+    return db.query(CountryModel).all()
 
 
 def sync_counties(
         db: Session,
-        countries_to_sync: List[schemas.CountryUpdate]
+        countries_to_sync: List[CountryUpdate]
 ):
     """
     Функция для сохранения курсов валют в базу.
@@ -129,7 +192,7 @@ def sync_counties(
     was_created_counter = 0
 
     for country_to_sync in countries_to_sync:
-        country: CountryModel = db.query(models.CountryModel).get(country_to_sync.country)
+        country: CountryModel = db.query(CountryModel).get(country_to_sync.country)
         if country:  # если такая страна нашлась, то обновляем ее актуальными данными
             update_object(
                 db=db,
--- main.py
+++ main.py
@@ -8,8 +8,7 @@ from database import engine, get_db
 from parser import CountryCurrencyParser, RateParser
 import uvicorn
 
-from crud import sync_currency_rates, get_currency_rates, sync_counties, get_countries, \
-    sync_and_get_currency_related_rates
+from crud import *
 from models import Base
 
 from create_update import create_object, update_object, get_object
@@ -27,29 +26,14 @@ def sync_and_get_currency_rates_endpoint(
         end_date: date,
         db: Session = Depends(get_db)
 ):
-    """
-    API для синхронизации курсов валют за указанный период дат между сайтом и базой данных.
-
-    Parameters
-    ----------
-    start_date: дата начала периода
-    end_date: дата конца периода
-    db: сессия
-
-    Returns
-    -------
-    Лист из объектов типа CurrencyRate (@todo изменить, чтобы был не Create)
-    """
     # @todo добавить проверку на то, что нельзя выбрать период более двух лет
     # @todo добавить значения по умолчанию для дат
-    parser = RateParser()
-    rates_to_sync = parser.parse(start_date, end_date)
-    was_updated_counter, was_created_counter = sync_currency_rates(db, rates_to_sync=rates_to_sync)
-    rates_from_db = get_currency_rates(db, start_date, end_date)
+    was_updated_counter, was_created_counter = sync_currency_rates(db, start_date, end_date)
+    currency_rates = get_currency_rates(db, start_date, end_date)
     result = {
         'was_updated': was_updated_counter,
         'was_created': was_created_counter,
-        'data': rates_from_db
+        'data': currency_rates
     }
     return result
 
@@ -60,95 +44,15 @@ def sync_and_get_currency_related_rates_endpoint(
         end_date: date,
         db: Session = Depends(get_db)
 ):
-    # обновление baserates
-    base_date = date(2020, 1, 1)
-    sync_and_get_currency_rates_endpoint(base_date, base_date, db)
-
-    currency_codes = {
-        'USD': 52148,  # доллар
-        'EUR': 52170,  # евро
-        'GBP': 52146,  # фунт стерлигов
-        'JPY': 52246,  # японская йена
-        'TRY': 52158,  # турецкая лира
-        'INR': 52238,  # индийская рупия
-        'CNY': 52207,  # китайский юань
+    was_updated_counter_base, was_created_counter_base = sync_base_currency_rates(db, start_date, end_date)
+    was_updated_counter_rel, was_created_counter_rel = sync_related_currency_rates(db, start_date, end_date)
+    currency_rates = get_related_currency_rates(db, start_date, end_date)
+    result = {
+        'was_updated': was_updated_counter_base + was_updated_counter_rel,
+        'was_created': was_created_counter_base + was_created_counter_rel,
+        'data': currency_rates
     }
-
-    for currency_code, _ in currency_codes.items():
-
-        query = select(CurrencyRateModel).where(CurrencyRateModel.date == base_date).where(
-            CurrencyRateModel.currency_code == currency_code)
-        result = db.execute(query)
-        currency: CurrencyRateModel = result.scalar_one_or_none()
-
-        param: Parameters = db.query(Parameters).get(currency_code)
-        if param:
-            new_parameter = ParameterUpdate(
-                base_rate=currency.rate,
-                date_of_base_rate=base_date,
-            )
-            update_object(
-                db=db,
-                model=Parameters,
-                obj_id=currency_code,
-                schema=new_parameter
-            )
-        else:
-            new_parameter = Parameter(
-                currency_code=currency_code,
-                base_rate=currency.rate,
-                date_of_base_rate=base_date,
-            )
-            create_object(
-                db=db,
-                model=Parameters,
-                schema=new_parameter
-            )
-
-    # query = select(Parameters).where(True)
-    # result = db.execute(query)
-    # temp = list(result.scalars())
-    # print(*temp)
-
-    # обновление курсов валют
-    related_rates_to_sync = sync_and_get_currency_rates_endpoint(start_date, end_date, db)['data']
-
-    # # обновление БД относительных курсов валют
-    for related_rate in related_rates_to_sync:
-        currency_code = related_rate.currency_code
-        param: Parameters = db.query(Parameters).get(currency_code)
-        if param:
-            related_rate_value = related_rate.rate / param.base_rate
-            query = select(RelatedCurrencyRateModel).where(
-                RelatedCurrencyRateModel.date == related_rate.date).where(
-                RelatedCurrencyRateModel.currency_code == currency_code)
-            result = db.execute(query)
-            existing_related_rate: RelatedCurrencyRateModel = result.scalar_one_or_none()
-
-            if existing_related_rate:
-                update_object(
-                    db=db,
-                    model=RelatedCurrencyRateModel,
-                    obj_id=(currency_code, related_rate.date),
-                    schema=CurrencyRelatedRateUpdate(
-                        related_rate=related_rate_value
-                    )
-                )
-            else:
-                create_object(
-                    db=db,
-                    model=RelatedCurrencyRateModel,
-                    schema=CurrencyRelatedRateCreate(
-                        currency_code=currency_code,
-                        date=related_rate.date,
-                        related_rate=related_rate_value
-                    )
-                )
-
-    return db.query(RelatedCurrencyRateModel).filter(
-        RelatedCurrencyRateModel.date >= start_date,
-        RelatedCurrencyRateModel.date <= end_date
-    ).all()
+    return result
 
 
 @app.post("/sync-and-get-countries/")
