c79b8bba80838b83e98e47e44a186742b24387da
kudr.max <mdkudryashov@edu.hse.ru>
2024-06-08 19:25:59 +0300

    Add timestamp for plots

--- app/controllers/plot_controller.py
+++ app/controllers/plot_controller.py
@@ -1,4 +1,6 @@
-from datetime import date
+import os
+import re
+from datetime import date, datetime
 
 import pandas as pd
 from matplotlib import pyplot as plt
@@ -23,7 +25,7 @@ class PlotController(BaseController):
             currency_data[currency_code]['countries'].append(country)
 
         plt.figure(figsize=(10, 6))
-        plt.title('Относительное изменение курсов валют', fontsize=16)
+        # plt.title('Относительное изменение курсов валют', fontsize=16)
         for currency_code, value in currency_data.items():
             data = value['data']
             countries = value['countries']
@@ -37,9 +39,25 @@ class PlotController(BaseController):
         plt.xlabel('Дата', fontsize=12)
         plt.ylabel('Относительное изменение курсов валют', fontsize=12)
         plt.grid(alpha=0.4)
-        plt.legend(title='Валюты', fontsize=10, title_fontsize=12, loc='lower right')
+        # plt.legend(title='Валюты', fontsize=10, title_fontsize=12, loc='lower right')
+        plt.legend(fontsize=10, title_fontsize=12, loc='lower right')
         plt.tight_layout()
-        plot_link = f'{start_date}-{end_date}-{"-".join(currency_data.keys())}.png'
+        # plot_link = f'{start_date}-{end_date}-{"-".join(currency_data.keys())}.png'
+        plot_link = f'{datetime.now().strftime("%Y%m%d-%H%M%S")}.png'
         plt.savefig(self.plot_dir + plot_link)
         plt.close()
+
+        # если в директории более 10 файлов с графиками, то удалить самый старый
+        # не самое элегантное решение, но пойдет как временный костыль
+        pattern = re.compile(r'\d{8}-\d{6}\.png')
+        files = [f for f in os.listdir(self.plot_dir) if os.path.isfile(os.path.join(self.plot_dir, f))]
+        plot_files = [f for f in files if pattern.match(f)]
+        max_files = 10
+        if len(plot_files) > max_files:
+            plot_files.sort(key=lambda x: os.path.getctime(os.path.join(self.plot_dir, x)))
+            files_to_delete = plot_files[:-max_files]
+            for file in files_to_delete:
+                os.remove(os.path.join(self.plot_dir, file))
+                print(f"Deleted plot image: {file}")
+
         return plot_link
